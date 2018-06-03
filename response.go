0a2b5142f6119afed1f9f35e3b5b3e5e0ec49e4e
Akshit Grover <akshit.grover2016@gmail.com>
2018-06-03 23:51:11 +0530

    Response: Add Status Header | Send Methods

--- response.go
+++ response.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 )
 
+//Send JSON Response
 func (r Response) Json(status int, v interface{}) error {
 	b, err := json.Marshal(v)
 	if err != nil {
@@ -13,4 +14,22 @@ func (r Response) Json(status int, v interface{}) error {
 	r.Res.WriteHeader(status)
 	r.Res.Write(b)
 	return nil
+}
+
+//Send HTML Response
+func (r Response) Send(status int, v string) {
+	r.Res.Header().Set("Content-Type", "text/html")
+	r.Res.WriteHeader(status)
+	r.Res.Write([]byte(v))
+}
+
+//Set Headers
+func (r Response) Set(k string, v string) {
+	r.Res.Header().Set(k, v)
+}
+
+//Send Response With Empty Body
+func (r Response) SendStatus(status int) {
+	r.Res.WriteHeader(status)
+	r.Res.Write([]byte(""))
 }
