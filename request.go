928d0579239ecd98dcf95c0e818148d898685cee
Akshit Grover <akshit.grover2016@gmail.com>
2018-06-06 20:42:46 +0530

    EssenRequest: Add Multipart Handler | Update requestBody Func

--- request.go
+++ request.go
@@ -1,9 +1,11 @@
 package essen
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type GetBody struct {
@@ -11,6 +13,10 @@ type GetBody struct {
 }
 
 type PostBody struct {
+	body *http.Request
+}
+
+type MultiPartBody struct {
 	body *http.Request
 }
 
@@ -41,6 +47,35 @@ func (b PostBody) Params(name string) (string, EssenError) {
 	return v, ee
 }
 
+func (b MultiPartBody) Params(name string) (string, EssenError) {
+	ee := EssenError{nilval: true}
+	file, fileHeader, err := b.body.FormFile(name)
+	if err != nil && err.Error() != "http: no such file" {
+		ee.nilval = false
+		ee.errortype = "FormParseError"
+		ee.message = err.Error()
+		return "", ee
+	} else if err == nil {
+		UploadDir := MultiPartConfig["UploadDir"]
+		path := UploadDir + "/" + fileHeader.Filename
+		f, ee := CreateFileIfNotExist(path)
+		if !ee.IsNil() {
+			return "", ee
+		}
+		n, err := io.Copy(f, file)
+		log.Println(n, err)
+		return path, ee
+	}
+	v := b.body.FormValue(name)
+	if v == "" {
+		ee.nilval = false
+		ee.errortype = "InvalidParam"
+		ee.message = `No parameter with key "` + name + `"`
+		return "", ee
+	}
+	return v, ee
+}
+
 func (r Request) Path() string {
 	return r.Req.URL.Path
 }
@@ -60,15 +95,32 @@ func (r Request) HasHeader(key string) bool {
 	}
 	return true
 }
+
 func (r Request) Header(key string) (string, EssenError) {
 	if r.HasHeader(key) {
-		return r.Req.Header.Get(key), EssenError{nilval: true}
+		hval := r.Req.Header.Get(key)
+		ok := strings.HasPrefix(hval, "multipart")
+		if ok {
+			hval = strings.Split(hval, ";")[0]
+		}
+		return hval, EssenError{nilval: true}
 	}
 	return "", EssenError{message: "No Header Found", errortype: "NoHeader", nilval: false}
 }
 
 func (r *Request) requestBody() {
-	if r.Method() == "GET" {
+	contentType, ee := r.Header("Content-Type")
+	if !ee.IsNil() {
+		log.Panic(ee.Error())
+	}
+	if contentType == "multipart/form-data" {
+		if !mConfigIsSet() {
+			setDefaultConfig()
+		}
+		r.Body = MultiPartBody{body: r.Req}
+		return
+	}
+	if r.Method() == "GET" || r.Method() == "HEAD" {
 		r.Body = GetBody{body: r.Req.URL}
 		return
 	}
