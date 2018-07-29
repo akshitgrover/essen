0b90b1ba38408acd3ed29bbe68d5138fba4671bc
Akshit Grover <akshit.grover2016@gmail.com>
2018-07-29 12:29:17 +0530

    EssenRouter: Add HTTP Put Method | Router Done Method

--- router.go
+++ router.go
@@ -8,6 +8,7 @@ import (
 type router struct {
 	get  handlerStorage
 	post handlerStorage
+	put  handlerStorage
 }
 
 func (r *router) Get(route string, f func(Response, Request)) {
@@ -42,6 +43,22 @@ func (r *router) Post(route string, f func(Response, Request)) {
 	r.post[route] = ff
 }
 
+func (r *router) Put(route string, f func(Response, Request)) {
+	ff := func(res http.ResponseWriter, req *http.Request) {
+
+		//Custom Response Fields
+		eres := Response{Res: res, ReqMethod: "PUT"}
+
+		//Csutom Request Fields
+		ereq := Request{Req: req, Uid: uid.New(7)}
+		ereq.requestBody()
+
+		//Call Registered Middleware
+		f(eres, ereq)
+	}
+	r.put[route] = ff
+}
+
 func (e Essen) UseRouter(route string, router router) {
 
 	//Register Get Routes
@@ -55,6 +72,16 @@ func (e Essen) UseRouter(route string, router router) {
 		froute := route + k
 		paths.post[froute] = v
 	}
+
+	//Register Put Routes
+	for k, v := range router.put {
+		froute := route + k
+		paths.put[froute] = v
+	}
+}
+
+func (r *router) Done() {
+	*r = router{}
 }
 
 func (e Essen) Router() router {
